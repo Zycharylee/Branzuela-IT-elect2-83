3f47b7d6e757697c4a9970a4bbf0615fb9ee5ef9
branzuela <branzuelazycharylee@gmail.com>
2023-03-22 19:20:14 +0800

    update

--- app/src/main/java/com/example/helloandroid/MainActivity1.java
+++ app/src/main/java/com/example/helloandroid/MainActivity1.java
@@ -2,6 +2,10 @@ package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
 import android.os.Bundle;
 
 public class MainActivity1 extends AppCompatActivity {
@@ -10,5 +14,13 @@ public class MainActivity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
+        Button button = (Button) findViewById(R.id.btn2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity1.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
